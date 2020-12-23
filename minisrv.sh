3f2d1e6fb0bab513a19dd6a45acd3fb1a6b9b67b
shermanx <shermanx@gmail.com>
2020-12-23 12:54:14 +1000

    Update minisrv.sh

--- minisrv.sh
+++ minisrv.sh
@@ -5,7 +5,12 @@ sudo killall mini_httpd
 sudo apt install mini-httpd
 mkdir ~/tmp
 cd ~/tmp
+dd if=/dev/zero of=50.bin bs=25MB count=2
 dd if=/dev/zero of=100.bin bs=25MB count=4
 mini_httpd -p 50080
-echo "Please use this cmd to test: wget http://`dig +short myip.opendns.com @resolver1.opendns.com`:50080/100.bin"
+echo
+echo "*** Please use these to test: "
+echo "*** wget http://`dig +short myip.opendns.com @resolver1.opendns.com`:50080/50.bin -v -O 50.bin"
+echo "*** wget http://`dig +short myip.opendns.com @resolver1.opendns.com`:50080/100.bin -v -O 100.bin"
+echo 
 
