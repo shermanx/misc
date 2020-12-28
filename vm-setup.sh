f8a262f0b1edd6083142a7067b2bac34a8b06c1f
shermanx <shermanx@gmail.com>
2020-12-28 18:51:49 +1000

    Update vm-setup.sh

--- vm-setup.sh
+++ vm-setup.sh
@@ -2,7 +2,8 @@
 # wget https://raw.githubusercontent.com/shermanx/misc/master/vm-setup.sh && chmod +x vm-setup.sh && ./vm-setup.sh
 
 sudo -i
-sudo apt update && sudo apt upgrade -y && sudo apt install net-tools speedtest-cli iftop nethogs traceroute
+sudo apt update && sudo apt upgrade -y && sudo apt install net-tools speedtest-cli iftop nethogs traceroute curl
+curl -fsL https://ilemonra.in/LemonBenchIntl | bash -s full
 sudo iptables -L
 sudo iptables-save > ~/iptables-rules
 sudo iptables -P OUTPUT ACCEPT
