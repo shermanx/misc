ce89f9835da393f9fcca12806c10a3b79b69026f
shermanx <shermanx@gmail.com>
2020-12-28 18:52:31 +1000

    Update vm-setup.sh

--- vm-setup.sh
+++ vm-setup.sh
@@ -4,6 +4,8 @@
 sudo -i
 sudo apt update && sudo apt upgrade -y && sudo apt install net-tools speedtest-cli iftop nethogs traceroute curl
 curl -fsL https://ilemonra.in/LemonBenchIntl | bash -s full
+
+#reset iptables
 sudo iptables -L
 sudo iptables-save > ~/iptables-rules
 sudo iptables -P OUTPUT ACCEPT
@@ -11,6 +13,10 @@ sudo iptables -P FORWARD ACCEPT
 sudo iptables -F
 sudo iptables -L
 sudo iptables-save > ~/iptables-rules.allow-all
+
+#install v2ray
 bash <(curl -s -L https://git.io/v2ray.sh)
-61286
+#port 61286
+
+#Monitor
 netstat -tanplc |grep 61286
