24a7257b751b71e63d464d28d4afe94aefbc1662
shermanx <shermanx@gmail.com>
2020-12-31 14:02:44 +1000

    Update vm-setup.sh

--- vm-setup.sh
+++ vm-setup.sh
@@ -1,10 +1,23 @@
 # To setup a fresh VM environ
 # wget https://raw.githubusercontent.com/shermanx/misc/master/vm-setup.sh && chmod +x vm-setup.sh && ./vm-setup.sh
 
-sudo -i
-sudo apt update && sudo apt upgrade -y && sudo apt install net-tools speedtest-cli iftop nethogs traceroute curl vnstat -y
+echo Update and Install? (y/N):
+read cmd
+if [ $cmd == 'y' ]
+then
+  sudo apt update && sudo apt upgrade -y && sudo apt install net-tools speedtest-cli iftop nethogs traceroute curl vnstat -y
+fi
 # curl -fsL https://ilemonra.in/LemonBenchIntl | bash -s full
 # wget -qO- git.io/besttrace | bash
+
+echo Setup bash profile? (y/N):
+read cmd
+if [ $cmd == 'y' ]
+then
+  echo What\'s your server name?
+  read sid
+  curl https://raw.githubusercontent.com/shermanx/misc/master/.profile |sed 's/YOUR_SERVER_ID/$sid/g' > .profile
+fi
 
 #reset iptables
 sudo iptables -L
@@ -16,6 +29,7 @@ sudo iptables -L
 sudo iptables-save > ~/iptables-rules.allow-all
 
 #install v2ray
+sudo -i
 bash <(curl -s -L https://git.io/v2ray.sh)
 #port 61286
 
